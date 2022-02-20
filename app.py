8d16d362e35aeb0ac2bb4fb0f6f6933468c90216
Juniper S <wildcat8088@live.com>
2022-02-20 05:33:30 +0000

    Move favicon to content directory

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app = publ.publ(__name__, config)
 @app.route('/favicon.ico')
 def favicon():
     """ Send the favicon.ico file directly from this directory """
-    return flask.send_file('favicon.ico')
+    return flask.redirect(flask.url_for('static', filename='favicon.ico'))
